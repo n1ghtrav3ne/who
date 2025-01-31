91d1304681b3c50a666ae5058152c2f0657048c2
C0DeB100D <@c0deb100d@>
2025-01-31 13:00:15 +0330

    finished the toggle component

--- components/global/TheToggle.vue
+++ components/global/TheToggle.vue
@@ -1,13 +1,27 @@
 <script lang="ts" setup>
-defineProps<{
-
+const props=defineProps<{
+  modelValue:boolean
 }>();
+
+const emit=defineEmits<{
+  (event:"update:modelValue",value:boolean):void
+}>();
+
+const toggle=()=>{
+  emit("update:modelValue",!props.modelValue);
+}
 </script>
 
 <template>
-  <div class="relative w-[48px] h-[26px] cursor-pointer bg-[#E9EAEE] rounded-full">
+  <div 
+    @click="toggle" 
+
+    class="relative w-[48px] h-[26px] cursor-pointer bg-[#E9EAEE] transition-colors duration-300 ease-out rounded-full"
+    :class="{'bg-neutral-500':modelValue}"
+  >
     <div 
-      class="absolute top-1/2 -translate-y-1/2 w-[18px] h-[18px] bg-[#AEB2C0] rounded-full left-1"
+      class="absolute top-1/2 -translate-y-1/2 w-[18px] h-[18px] bg-[#AEB2C0] rounded-full transition-all duration-300 ease-out"
+      :class="{'left-1':!modelValue,'left-6 bg-neutral-100':modelValue}"
     ></div>
   </div>
 </template>
