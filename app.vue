41a93965eed2f8e84d35a708d8324280c5b2985b
C0DeB100D <@c0deb100d@>
2025-01-31 14:01:52 +0330

    added events and active style

--- app.vue
+++ app.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+const currentIndex=ref(1);
+
+const updateList=(i:number)=>{
+  console.log(i);
+  
+}
 
 </script>
 
 <template>
   <div class="mt-10 mx-auto w-[600px]">
-    <TheToggle />
+    <ThePagination :count="7" v-model:current="currentIndex" @change="updateList" />
   </div>
 </template>
 
--- components/global/ThePagination.vue
+++ components/global/ThePagination.vue
@@ -3,24 +3,43 @@ import type { PaginationProps } from '~/types/Pagination';
 
 const props=defineProps<PaginationProps>();
 
+const emit=defineEmits<{
+    (event:'update:current',value:number):void,
+    (event:'change',value:number):void,
+}>();
+
+const changeIndex=(i:number)=>{
+    emit("update:current",i);
+    emit("change",i);
+}
 </script>
 
 <template>
     <div class="flex gap-2 items-center">
-        <div class="cursor-pointer border flex justify-center items-center w-8 h-8 text-center border-[#E9EAEE] rounded-lg">
+        <div
+            :class="{'cursor-default text-neutral-200':current<=1}"
+            class="cursor-pointer border flex justify-center items-center w-8 h-8 text-center border-[#E9EAEE] rounded-lg"
+        >
             <!-- TODO: add icon --> 
              R
         </div>
 
         <div class="flex gap-2 text-md text-[#5B6176] cursor-pointer">
             <div 
+                v-for="i in count"
+                @click="changeIndex(i)"
+
+                :class="{'bg-black text-white border-black':current==i}"
                 class="border flex justify-center items-center w-8 h-8 text-center border-[#E9EAEE] rounded-lg"
             >
-                1
+                {{i}}
             </div>
         </div>
 
-        <div class="cursor-pointer border flex justify-center items-center w-8 h-8 text-center border-[#E9EAEE] rounded-lg">
+        <div
+            :class="{'cursor-default text-neutral-200':current>=count}"
+            class="cursor-pointer border flex justify-center items-center w-8 h-8 text-center border-[#E9EAEE] rounded-lg"
+        >
             <!-- TODO: add icon -->
              L
         </div>
