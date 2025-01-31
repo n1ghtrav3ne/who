19d4a7927c5d48eb1bd7d5dab3579fea5599aef1
C0DeB100D <@c0deb100d@>
2025-01-31 15:10:29 +0330

    cleared app.vue

--- app.vue
+++ app.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
-const currentIndex=ref(3);
 
-const updateList=(i:number)=>{
-  console.log(i/5);
-  
-}
 
 </script>
 
 <template>
-  <div class="mt-10 mx-auto w-[600px]">
-    <ThePagination :count="15" v-model:current="currentIndex" :groupSize="5" @change="updateList" />
-  </div>
+  
 </template>
 
 <style>
