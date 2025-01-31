f5bc7b9328a5f7031b35532dafc7470ea189e83a
C0DeB100D <@c0deb100d@>
2025-01-31 12:39:48 +0330

    added style to component

--- app.vue
+++ app.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  
+  <div class="mt-10 mx-auto w-[600px]">
+    <TheToggle />
+  </div>
 </template>
 
 <style>
--- components/global/TheToggle.vue
+++ components/global/TheToggle.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup>
+defineProps<{
 
+}>();
 </script>
 
 <template>
-  
+  <div class="relative w-[48px] h-[26px] cursor-pointer bg-[#E9EAEE] rounded-full">
+    <div 
+      class="absolute top-1/2 -translate-y-1/2 w-[18px] h-[18px] bg-[#AEB2C0] rounded-full left-1"
+    ></div>
+  </div>
 </template>
