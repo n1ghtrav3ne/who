7d46a6fc76e9e559efbcb333ca7c2693fdcfa1a5
C0DeB100D <@c0deb100d@>
2025-01-31 11:53:27 +0330

    added neutral color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
     './nuxt.config.{js,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        neutral:{
+          50:"#f6f6f6",
+          100:"#e7e7e7",
+          200:"#d1d1d1",
+          300:"#b0b0b0",
+          400:"#888888",
+          500:"#6d6d6d",
+          600:"#5d5d5d",
+          700:"#4f4f4f",
+          800:"#454545",
+          900:"#3d3d3d",
+          950:"#0a0a0a",
+        }
+      }
+    },
   },
   plugins: [],
 }
