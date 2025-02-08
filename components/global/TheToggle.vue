8b61c69e26dbd698f03e38d40d3d921dd46051c0
C0DeB100D <@c0deb100d@>
2025-02-08 10:39:20 +0330

    simplified component by using defineModel

--- components/global/TheToggle.vue
+++ components/global/TheToggle.vue
@@ -1,14 +1,13 @@
 <script lang="ts" setup>
-const props=defineProps<{
-  modelValue:boolean
-}>();
+
+const model=defineModel<boolean>({"required":true})
 
 const emit=defineEmits<{
   (event:"update:modelValue",value:boolean):void
 }>();
 
 const toggle=()=>{
-  emit("update:modelValue",!props.modelValue);
+  model.value=!model.value;
 }
 </script>
 
@@ -17,11 +16,11 @@ const toggle=()=>{
     @click="toggle" 
 
     class="relative w-[48px] h-[26px] cursor-pointer bg-[#E9EAEE] transition-colors duration-300 ease-out rounded-full"
-    :class="{'bg-neutral-500':modelValue}"
+    :class="{'bg-neutral-500':model}"
   >
     <div 
       class="absolute top-1/2 -translate-y-1/2 w-[18px] h-[18px] bg-[#AEB2C0] rounded-full transition-all duration-300 ease-out"
-      :class="{'left-1':!modelValue,'left-6 bg-neutral-100':modelValue}"
+      :class="{'left-1':!model,'left-6 bg-neutral-100':model}"
     ></div>
   </div>
 </template>
