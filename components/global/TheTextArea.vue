bac0683c102f52098bd7991f864b489661c5a439
C0DeB100D <@c0deb100d@>
2025-02-08 12:45:42 +0330

    created TextArea component

---
+++ components/global/TheTextArea.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const model=defineModel<string>({required:true});
+
+const props=defineProps<{
+    placeholder?:string
+}>();
+</script>
+
+<template>
+    <textarea 
+        class="h-32 resize-y p-2
+            border border-neutral-950 outline-none
+            hover:border-2 focus:border-2"
+
+        :placeholder="placeholder"
+        v-model="model"></textarea>
+</template>
