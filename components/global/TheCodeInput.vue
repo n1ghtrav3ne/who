186a202e0bad4de7c354615c561f512d48dc5735
C0DeB100D <@c0deb100d@>
2025-02-13 16:45:01 +0330

    added code inputs

---
+++ components/global/TheCodeInput.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { CodeInputProps } from '~/types/components/CodeInput';
+
+const model=defineModel<string>();
+const props=defineProps<CodeInputProps>();
+
+const inputs=ref<Array<HTMLInputElement|null>>(Array(props.maxCharachters/props.characterPerInput));
+
+const changeInput=(event:KeyboardEvent)=>{
+    const target=event.target as HTMLInputElement;
+
+    if(target.value.length!=props.characterPerInput){
+        return;
+    }
+
+    const currentInputIndex=inputs.value.indexOf(target);
+
+    if(!model.value)
+    {
+        model.value="";
+    }
+    console.log(inputs.value[currentInputIndex]?.value);
+    model.value+=inputs.value[currentInputIndex]?.value;
+
+    if(currentInputIndex<inputs.value.length-1){
+        inputs.value[currentInputIndex + 1]?.focus();
+    }
+}
+</script>
+
+<template>
+    <div class="flex gap-2" dir="ltr">
+        <div class="min-w-28 w-6" v-for="(item,i) in maxCharachters/characterPerInput" >
+            <input
+                    :ref="(el)=>inputs[i]=(el as HTMLInputElement)"
+                    :maxlength="characterPerInput"
+                    @keyup="changeInput($event)"
+                    type="text" 
+                    class="border-b text-center outline-none px-2 w-full py-1">
+        </div>
+    </div>
+</template>
