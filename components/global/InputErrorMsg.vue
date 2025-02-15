424644b0a32fd145b375161f00c74f19a84a45e3
C0DeB100D <@c0deb100d@>
2025-02-15 16:29:08 +0330

    added a new component for inputs with error message

---
+++ components/global/InputErrorMsg.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    const props=defineProps<{
+        text:string
+    }>();
+</script>
+
+<template>
+    <div class="text-error-dark flex gap-1 items-center text-xs">
+        <span class="pt-2">*</span>
+        <span>{{ text }}</span>
+    </div>
+</template>
