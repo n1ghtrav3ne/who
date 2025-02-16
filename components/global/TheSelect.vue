38c8c137ef95cb457810e96d9731a303bcdd4e2f
C0DeB100D <@c0deb100d@>
2025-02-16 09:02:30 +0330

    created select component

---
+++ components/global/TheSelect.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import type { SelectProps } from '~/types/components/Select';
+
+const props=defineProps<SelectProps>();
+const model=defineModel({required:true});
+
+const isOpen=ref<boolean>(false);
+const currentOption=ref<string>();
+
+const selectOption=(label:string,value:string)=>{
+    model.value=value;
+    currentOption.value=label;
+    isOpen.value=false;
+}
+</script>
+
+<template>
+    <div class="cursor-pointer select-none w-72 flex flex-col gap-2">
+        <div>
+            عنوان
+        </div>
+
+        <div @click="isOpen=!isOpen" class="relative border border-black py-1 flex justify-around items-center">
+            <div>
+                ic
+            </div>
+
+            <div class="text-neutral-400">
+                {{currentOption || defaultLabel}}
+            </div>
+
+            <div v-if="isOpen" @click.stop class="absolute w-full z-10 bg-white top-9 border border-black flex flex-col gap-4 px-4 py-2">
+                <!-- Returns the handler so programmer can change the value -->
+                <slot name="options" :selectOption="selectOption" />
+            </div>
+
+            <div>
+                ar
+            </div>
+        </div>
+
+        <div v-if="bottomLabel" class="text-neutral-300">
+            {{bottomLabel}}
+        </div>
+    </div>
+</template>
