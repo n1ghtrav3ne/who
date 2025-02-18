fa996d0a828337e0677442470f83659be3993e47
Amir <amirhoseim1344@gmail.com>
2025-02-18 19:24:23 +0330

    both footers for desktop and mobile are done

---
+++ components/global/DesktopFooter.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 w-full px-10 pb-8 pt-[72px]">
+
+    <div class="flex flex-row justify-between items-center text-[16px]">
+
+      <div class="flex flex-row items-center gap-10">
+
+        <span>اینستاگرام</span>
+
+        <span>پشتیبانی</span>
+
+      </div>
+
+      <span>
+        WHO
+      </span>
+
+      <div class="flex flex-row items-center gap-10">
+
+        <span>تماس با ما</span>
+
+        <span>درباره ما</span>
+
+      </div>
+
+    </div>
+
+    <div class="flex flex-row justify-center gap-10 font-light">
+
+      <span>آدرس: کرمان - بلوارنصر - اول حمزه شمالی</span>
+
+      <span>0913 199 9600</span>
+
+    </div>
+
+  </div>
+</template>
