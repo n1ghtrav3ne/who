0af6ffb25f99eb40e08b0e14f3405e23a368394b
Amir <amirhoseim1344@gmail.com>
2025-02-23 23:47:07 +0330

    the landing page is done

--- components/landing/LandingPage.vue
+++ components/landing/LandingPage.vue
@@ -1,25 +1,33 @@
 <script setup lang="ts">
-import model from "~/assets/image/model1.png"
+import model from "assets/image/model1.png";
+import model2 from "assets/image/model2.png";
+import Achievements from "~/components/landing/Achievements.vue";
+import Discount from "~/components/landing/Discount.vue";
+import Products from "~/components/landing/Products.vue";
 </script>
 
 <template>
-  <div class="container flex flex-col">
+  <div class="container mx-auto flex flex-col gap-10">
 
     <span class="text-[56px] font-light text-center">کالکشن  جدید “هو”</span>
 
-    <div class="grid grid-cols-3 gap-2 h-[400px] w-full">
+    <Products />
 
-      <poster :img="model" />
-      <poster :img="model" />
-      <poster :img="model" />
-      <poster :img="model" />
-      <poster :img="model" />
+    <span class="text-2xl font-light">چرا هو؟</span>
+
+    <Achievements />
+
+    <Discount />
+
+    <span class="text-[20px] font-light text-center lg:text-2xl">دسته بندی ها</span>
+
+    <div class="grid grid-cols-1 lg:grid-cols-3 w-full gap-8 h-[700px]">
+
+      <Poster class="bg-center" :img="model2" />
+      <Poster class="bg-center" :img="model" />
+      <Poster class="bg-center" :img="model2" />
 
     </div>
 
   </div>
 </template>
-
-<style scoped>
-
-</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,8 @@
 
 </script>
 <template>
-  <div class="flex flex-col min-h-screen">
     <NavBar />
     <NuxtPage />
     <DesktopFooter class="hidden lg:flex" />
     <MobileFooter class="flex lg:hidden" />
-  </div>
 </template>
