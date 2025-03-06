2ff52c7f08d179ed3ef89a32424f8409d42fb97f
Amir <amirhoseim1344@gmail.com>
2025-03-06 13:43:14 +0330

    main collection page is done

---
+++ components/collection/Content.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import male from "~/assets/image/male.jpg"
+
+import female from "~/assets/image/female.jpg"
+
+import slide from "~/assets/image/slide.jpg"
+</script>
+
+<template>
+  <div class="grid grid-cols-2 w-full gap-y-5 mt-5">
+
+    <Poster :img="male">
+      <div class="w-full h-full border flex flex-col items-center relative justify-center text-neutral-50">
+
+        <span class="text-[16px] lg:text-[64px] font-light">
+          کالکشن جدید ۲۰۲۵
+        </span>
+
+        <span class="text-sm lg:text-lg underline cursor-pointer relative top-[30%]">
+          خرید کنید
+        </span>
+
+      </div>
+    </Poster>
+
+    <Poster :img="female">
+      <div class="w-full h-full border flex flex-col items-center relative justify-center text-neutral-50">
+
+        <span class="text-[16px] lg:text-[64px] font-light">
+          کالکشن جدید ۲۰۲۵
+        </span>
+
+        <span class="text-sm lg:text-lg underline cursor-pointer relative top-[30%]">
+          خرید کنید
+        </span>
+
+      </div>
+    </Poster>
+
+    <Poster :img="slide" class="col-span-2">
+      <span class="w-full h-full flex justify-center items-center text-2xl lg:text-[64px] font-light text-neutral-50">
+        کالکشن جدید ۲۰۲۵
+      </span>
+    </Poster>
+
+  </div>
+</template>
---
+++ pages/collection/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+import CollectionPage from "~/components/collection/CollectionPage.vue";
+</script>
+
+<template>
+  <CollectionPage />
+</template>
