ddb6fc48a17ce5abcc9fff4572abaa396b08e8fc
Amir <amirhoseim1344@gmail.com>
2025-03-06 13:43:14 +0330

    main collection page is done

---
+++ components/collection/CollectionPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+import SlideShow from "~/components/collection/SlideShow.vue";
+import Content from "~/components/collection/Content.vue";
+</script>
+
+<template>
+  <SlideShow />
+  <Content />
+</template>
