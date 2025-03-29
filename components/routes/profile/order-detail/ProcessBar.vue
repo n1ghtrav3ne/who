8d7f89e8ef8f977328d8a5119c61d26f5b444e5e
Amir <amirhoseim1344@gmail.com>
2025-03-29 19:13:00 +0330

    the order detail page is done

---
+++ components/routes/profile/order-detail/ProcessBar.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+
+type OrderStatus = 'pending' | 'delivered' | 'returned';
+
+const props = defineProps<{
+  status: OrderStatus;
+}>();
+
+</script>
+
+<template>
+  <div class="container flex flex-col gap-3 w-full lg:w-[50%]">
+
+
+      <div class="w-full h-[8px] bg-neutral-100 overflow-hidden">
+
+        <div
+            class="h-full"
+            :class="{
+              'bg-neutral-950 w-1/2': status === 'pending',
+              'bg-success-light w-full': status === 'delivered',
+              'bg-error-light w-full': status === 'returned'
+            }"
+        ></div>
+      </div>
+
+
+    <div v-if="status==='delivered'" class="flex flex-row justify-between items-center">
+
+      <div class="text-sm text-success-light">
+        تحویل به مشتری
+      </div>
+
+      <span class="material-symbols-outlined text-success-light">
+      check_circle
+      </span>
+
+    </div>
+
+    <div v-if="status==='pending'" class="flex flex-row justify-between items-center">
+
+      <div class="text-sm text-neutral-950">
+        در حال پردازش...
+      </div>
+
+      <span class="material-symbols-outlined">
+      cycle
+      </span>
+
+    </div>
+
+    <div v-if="status==='returned'" class="flex flex-row justify-between items-center">
+
+      <div class="text-sm text-error-light">
+        لغو شده
+      </div>
+
+      <span class="material-symbols-outlined text-error-light">
+      cancel
+      </span>
+
+    </div>
+
+
+  </div>
+</template>
---
+++ components/routes/profile/orders/ProcessStatus.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+type ProcessStatus = 'pending' | 'delivered' | 'returned';
+
+const props = defineProps<{
+  status: ProcessStatus;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-row items-center gap-3 w-full">
+
+    <span v-if="props.status==='pending'" class="text-sm lg:text-[16px]">
+      در حال پردازش
+    </span>
+
+
+    <span v-if="props.status==='delivered'" class="material-symbols-outlined text-green-600">
+    check_circle
+    </span>
+
+    <span v-if="props.status==='delivered'" class="text-sm lg:text-[16px]">
+      تحویل شده
+    </span>
+
+
+    <span v-if="props.status==='returned'" class="material-symbols-outlined text-error-light">
+    cycle
+    </span>
+
+    <span v-if="props.status==='returned'" class="text-sm lg:text-[16px]">
+      مرجوع شده
+    </span>
+
+  </div>
+</template>
