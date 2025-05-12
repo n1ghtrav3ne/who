fffe66997c0a63f1a34e7bfe7ad221f728e36b63
C0DeB100D <c0deb100d.me@gmail.com>
2025-05-12 14:26:50 +0330

    fixed image and logo thing with favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     "~/assets/css/fonts.css",
   ],
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', "nuxt-swiper"],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', "nuxt-swiper", "@nuxt/image"],
 
   app:{
     head:{
